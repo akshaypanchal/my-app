acf7b5dcc79e40d75168725543be3cfcc74449da
Akshaykumar Panchal <akspanch@WKMZTDE16D0F.global.publicisgroupe.net>
2022-02-09 13:44:40 -0500

    communication setup between react native and webview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+
+export default function App() {
+
+  const [data, setData]= useState('');
+
+  useEffect(() => {
+    function handleEvent(message) {
+      console.log(message.data);
+      setData('React Native Event '+message.data.message+' '+message.data.triggeredAt);
+    }
+
+    document.addEventListener("reactNative", handleEvent);
+    window.addEventListener("reactNative", handleEvent);
+
+    return () =>
+      document.removeEventListener("reactNative", handleEvent);
+  }, []);
+
+const enableBioMetric = () => {
+    window.ReactNativeWebView.postMessage('BiometricAuthentication')
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Webview Screen</h1>
+      <h1>Enable FaceID</h1>
+      <button onClick={enableBioMetric}>Unlock with FaceID</button>
+      <></>
+      {data?`${data}`:''}
     </div>
   );
 }
-
-export default App;
